8762adc9a94a8bee601225106e405856df49765b
Ashok Kumar <ap271449@gmail.com>
2023-05-17 21:24:19 +0530

    updated url

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://auth-five-mauve.vercel.app', credentials: true }));
 
 
 // mongo store
